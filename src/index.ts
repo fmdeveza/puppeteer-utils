7674558a9a3b7140f67d0024306dc86424cff606
Fernando Deveza <fernandomdx3@gmail.com>
2026-01-12 22:39:55 -0300

    dotenv file

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import 'dotenv/config';
 import puppeteer from 'puppeteer';
 
 import options from './options';
 
 async function main() {
+  // console.log(process.env);
   const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
   await page.goto('https://developer.chrome.com/');
