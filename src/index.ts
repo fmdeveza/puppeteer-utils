a7cd81927efa2e0a61adbb89aea0674a3927aa8f
Fernando Deveza <fernandomdx3@gmail.com>
2026-01-27 23:01:33 -0300

    wait functions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 import 'dotenv/config';
 import puppeteer from 'puppeteer';
+import type { Page } from 'puppeteer';
 
 import options from './options';
+
+
+const wait = async (ms: number = 3000) => {
+  console.log("Waiting for", ms/1000, "seconds...");
+  await new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const wait4ever = async () => {
+  console.log("Waiting forever...");
+  await new Promise(() => {});
+}
 
 async function main() {
   // console.log(process.env);
