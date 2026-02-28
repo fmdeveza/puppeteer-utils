598d8e72a80f05124f979205cc353bd2da71d7fd
Fernando Deveza <fernandomdx3@gmail.com>
2026-02-28 11:24:51 -0300

    add puppeteer-extra and adblock extension

--- src/launch.ts
+++ src/launch.ts
@@ -1,8 +1,14 @@
-import puppeteer, { Browser, Page } from 'puppeteer';
+import puppeteer from 'puppeteer-extra';
+import { Browser, Page } from 'puppeteer';
+import AdblockerPlugin from 'puppeteer-extra-plugin-adblocker';
+
 import options from './options';
 
 const launch = async (): Promise<[Browser, Page]> => {
   console.log("Puppeteer options:", options);
+
+  puppeteer.use(AdblockerPlugin({ blockTrackers: true }));
+  
   const browser = await puppeteer.launch(options);
   const page = await browser.newPage();
 
