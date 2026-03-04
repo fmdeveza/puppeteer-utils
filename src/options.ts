e52d1671b3c58f7965516a57f372427eb22d2103
Fernando Deveza <fernandomdx3@gmail.com>
2026-03-03 21:13:09 -0300

    CHROME_BIN env to executablePath

--- src/options.ts
+++ src/options.ts
@@ -3,10 +3,16 @@ import type { LaunchOptions } from 'puppeteer';
 const IS_PROD = process.env.NODE_ENV === 'production';
 
 const REMOTE_DEBUG = process.env.REMOTE_DEBUG === "true" || process.env.REMOTE_DEBUG === "1"
-const CHROME_USER_AGENT = process.env.CHROME_USER_AGENT || 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
-const CHROME_LANG = process.env.CHROME_LANG || 'en-US';
+const CHROME_USER_AGENT = process.env.CHROME_USER_AGENT || "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+const CHROME_LANG = process.env.CHROME_LANG || "en-US";
+const CHROME_BIN = process.env.CHROME_BIN || "";
 
 const options: LaunchOptions = {
+  defaultViewport: null,
+  userDataDir: "./profile",
+  ...(CHROME_BIN && { executablePath: CHROME_BIN }),
+  headless: IS_PROD ? true : false,
+  devtools: IS_PROD ? false : true,
   slowMo: 10,
   args: [
       "--use-gl=egl",
@@ -23,11 +29,6 @@ const options: LaunchOptions = {
       `--user-agent=${CHROME_USER_AGENT}`,
       `--lang=${CHROME_LANG}`,
     ],
-    defaultViewport: null,
-    headless: IS_PROD ? true : false,
-    // headless: "shell",
-    devtools: IS_PROD ? false : true,
-    userDataDir: "./profile"
 };
 
 if (REMOTE_DEBUG) {
