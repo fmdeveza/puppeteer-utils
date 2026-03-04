dcbcf9455f11a7287ccfc959b26e675647ca46a6
Fernando Deveza <fernandomdx3@gmail.com>
2026-03-04 19:58:45 -0300

    options more idiomatic js

--- src/options.ts
+++ src/options.ts
@@ -1,9 +1,8 @@
 import type { LaunchOptions } from 'puppeteer';
 
 const IS_PROD = process.env.NODE_ENV === 'production';
-
-const REMOTE_DEBUG = process.env.REMOTE_DEBUG === "true" || process.env.REMOTE_DEBUG === "1"
-const CHROME_USER_AGENT = process.env.CHROME_USER_AGENT || "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+const REMOTE_DEBUG = process.env.REMOTE_DEBUG === "true" || process.env.REMOTE_DEBUG === "1";
+const CHROME_USER_AGENT = process.env.CHROME_USER_AGENT || "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36";
 const CHROME_LANG = process.env.CHROME_LANG || "en-US";
 const CHROME_BIN = process.env.CHROME_BIN || "";
 
@@ -11,35 +10,26 @@ const options: LaunchOptions = {
   defaultViewport: null,
   userDataDir: "./profile",
   ...(CHROME_BIN && { executablePath: CHROME_BIN }),
-  headless: IS_PROD ? true : false,
-  devtools: IS_PROD ? false : true,
+  headless: IS_PROD,
+  devtools: !IS_PROD,
   slowMo: 10,
   args: [
-      "--use-gl=egl",
-      "--no-sandbox",
-      "--disable-setuid-sandbox",
-      "--disable-dev-shm-usage",
-      "--disable-gpu",
-      "--disable-notifications",
-      "--window-size=1280,720",
-      "--disable-blink-features=AutomationControlled",
-      "--disable-features=IsolateOrigins,site-per-process",
-      "--no-first-run",
-      "--no-default-browser-check",
-      `--user-agent=${CHROME_USER_AGENT}`,
-      `--lang=${CHROME_LANG}`,
-    ],
+    "--use-gl=egl",
+    "--no-sandbox",
+    "--disable-setuid-sandbox",
+    "--disable-dev-shm-usage",
+    "--disable-gpu",
+    "--disable-notifications",
+    "--window-size=1280,720",
+    "--disable-blink-features=AutomationControlled",
+    "--disable-features=IsolateOrigins,site-per-process",
+    "--no-first-run",
+    "--no-default-browser-check",
+    `--user-agent=${CHROME_USER_AGENT}`,
+    `--lang=${CHROME_LANG}`,
+    ...(REMOTE_DEBUG ? ["--remote-debugging-address=0.0.0.0", "--remote-debugging-port=9222"] : []),
+    ...(process.env.VPN_HOST ? [`--proxy-server=${process.env.VPN_HOST}`] : []),
+  ],
 };
-
-if (REMOTE_DEBUG) {
-  options.args?.push(
-    "--remote-debugging-address=0.0.0.0",
-    "--remote-debugging-port=9222"
-  );
-}
-
-if (process.env.VPN_HOST) {
-  options.args?.push(`--proxy-server=${process.env.VPN_HOST}`);
-}
 
 export default options;
